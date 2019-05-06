e81c41369f885d59dee0db789cf3e8f5497a7a8e
MrWEI-google66 <epay.nit@gmail.com>
2019-05-06 22:24:34 +0800

    简书有介绍

---
+++ Project/RTE/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'onenetdemo' 
+ * Target:  'OneNET_Demo_ESP8266_HTTP_HT' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+#endif /* RTE_COMPONENTS_H */
